93c6d6fbcbb6a617bc2f82cbf45eefc9cd37c5f6
abadeerafif <abadeer@hotmail.com>
2022-06-15 01:57:49 +0200

    make logic like mobile for mail

--- src/AWS/maillogic.js
+++ src/AWS/maillogic.js
@@ -2,32 +2,40 @@ import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses"; // ES Modules
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 
-const identitypoolid = process.env.REACT_APP_IDENTITY_POOL_ID;
-const reg = process.env.REACT_APP_REGION;
-const COGNITO_IDP = process.env.REACT_APP_COGNITO_IDP;
-const src = process.env.REACT_APP_SOURCE;
-const config = {
-  region: reg,
-  credentials: fromCognitoIdentityPool({
-    client: new CognitoIdentityClient({ region: reg }),
-    identityPoolId: identitypoolid,
-  }),
-};
-export async function sendmail(Destination, Message, Source) {
-  const client = new SESClient(config);
-  const command = new SendEmailCommand({
-    Destination: { ToAddresses: [Destination] },
-    Message: {
-      Body: {
-        Text: {
-          Data: "TEXT_FORMAT_BODY",
-        },
-      },
-      Subject: {
-        Data: "EMAIL_SUBJECT",
-      },
-    },
-    Source: src,
-  });
-  const response = await client.send(command);
+const identitypoolid = "us-east-1:2b404e3d-6bdf-404a-8f21-701f364fb12f";
+const reg = "us-east-1";
+
+const config={
+    region: reg,
+    credentials: fromCognitoIdentityPool({
+        client: new CognitoIdentityClient({ region: reg }),
+        identityPoolId: identitypoolid,
+        
+    })
+}
+export async function sendmail(Destination,type,id)
+{
+    const client = new SESClient(config);
+    const command = new SendEmailCommand({
+        Destination:{ToAddresses:[Destination]},
+        Message:{
+           
+            Body: {
+                Text: {
+                   
+                    Data: "Your "+type+" "+id+" has been found please sign in to your account see more information",
+                  }
+              
+            },
+            Subject: {
+                
+                Data: "Lost and Found Notify",
+              
+              
+            },
+          },
+        Source:"findlostgradprojectfcis@outlook.com"
+    });
+    const response = await client.send(command);
+
 }
